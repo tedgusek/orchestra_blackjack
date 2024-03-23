95d5769c05df12befd3431c18cda252d5e50875d
Ted Gusek <theohgusek@gmail.com>
2024-03-23 14:19:23 -0400

    game logic handling who wins etc

---
+++ src/utils/blackjackLogic.ts
@@ -0,0 +1,6 @@
+// Player wins if playerHand is less than 21 and greater than houseHand total
+// Player wins if playerHand is 21 and houseHand total is not
+
+// House wins if playerHand is over 21 --Consider the Ace 1 or 11
+// House wins if playerHand total is less than 21 and less than houseHand total
+// House wins in the event of a tie

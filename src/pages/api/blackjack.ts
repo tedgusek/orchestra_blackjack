ab7c6479882f3318423215ce18f88ba30aa32666
Ted Gusek <theohgusek@gmail.com>
2024-03-23 12:52:17 -0400

    implemented shuffle the deck using the same deck, implemented hit button

--- src/pages/api/blackjack.ts
+++ src/pages/api/blackjack.ts
@@ -70,25 +70,3 @@ export const shuffleCards = async (
     console.error('Error Shuffling: ', error);
   }
 };
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<BlackjackApiResponse>
-// ) {
-//   if (req.method === 'GET') {
-//     try {
-//       const { deck_id } = await initDeck();
-//       const initialHouseHand = await dealCard(deck_id, 2);
-//       const initialPlayerHand = await dealCard(deck_id, 2);
-//       const response: BlackjackApiResponse = {
-//         deckId: deck_id,
-//         playerHand: initialPlayerHand.cards,
-//         houseHand: initialHouseHand.cards,
-//       };
-//       res.status(200).json(response);
-//     } catch (error) {
-//       res.status(500).json({ message: 'Internal Server Error' });
-//     }
-//   } else {
-//     res.status(405).json({ message: 'Method Not Allowed' });
-//   }
-// }
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -7,60 +7,94 @@ import {
 import Hand from '../../components/Hand';
 import Button from '../../components/Button';
 
-// interface Card {
-//   code: string;
-//   image: string;
-//   value: string;
-//   suit: string;
-// }
-
-// interface DealCardResponse {
-//   cards: Card[];
-// }
-
 const Game = () => {
   const [deck_id, setDeckId] = useState<string | null>(null);
   //   const [playerHand, setPlayerHand] = useState<DealCardResponse>();
   const [playerHand, setPlayerHand] = useState<string[]>([]);
   const [houseHand, setHouseHand] = useState<string[]>([]);
 
-  //   const startGame = async () => {
-  useEffect(() => {
-    const startGame = async () => {
-      const { deck_id } = await initDeck();
-      setDeckId(deck_id);
-      console.log('deck_id: ', deck_id);
-      const initialHouseHand = await dealCard(deck_id, 2);
-      console.log('initialHouseHand: ', initialHouseHand);
-      const initialPlayerHand = await dealCard(deck_id, 2);
-      console.log('initialPlayerHand: ', initialPlayerHand);
+  const startGame = async () => {
+    const { deck_id } = await initDeck();
+    setDeckId(deck_id);
+    console.log('deck_id: ', deck_id);
+    const initialHouseHand = await dealCard(deck_id, 2);
+    console.log('initialHouseHand: ', initialHouseHand);
+    const initialPlayerHand = await dealCard(deck_id, 2);
+    console.log('initialPlayerHand: ', initialPlayerHand);
 
-      setHouseHand(initialHouseHand.cards);
-      console.log('initialHouseHand.cards : ', initialHouseHand.cards);
-      setPlayerHand(initialPlayerHand.cards);
-    };
+    setHouseHand(initialHouseHand.cards);
+    console.log('initialHouseHand.cards : ', initialHouseHand.cards);
+    setPlayerHand(initialPlayerHand.cards);
+  };
+
+  useEffect(() => {
     startGame();
   }, []);
 
-  // Implement Hit and Stand Functionality
-  // Have Start game only render before first game, after that it shouldn't be visible
-  // Need to implement a shuffle button, that will take all cards, and shufflle them back into the deck
+  const hitPlayer = async () => {
+    if (deck_id) {
+      try {
+        const response = await dealCard(deck_id, 1);
+        setPlayerHand([...playerHand, ...response.cards]);
+      } catch (error) {
+        console.error('Error hitting Player : ', error);
+      }
+    } else {
+      console.error('No Deck Available');
+    }
+  };
 
-  // useEffect(startGame)
+  // Shuffles deck but does not handle any scores yet
+  const shuffleDeck = async () => {
+    if (deck_id) {
+      try {
+        const response = await shuffleCards(deck_id);
+        // setPlayerHand([...playerHand, ...response.cards]);
+        const initialHouseHand = await dealCard(deck_id, 2);
+        console.log('initialHouseHand: ', initialHouseHand);
+        const initialPlayerHand = await dealCard(deck_id, 2);
+        console.log('initialPlayerHand: ', initialPlayerHand);
+
+        setHouseHand(initialHouseHand.cards);
+        console.log('initialHouseHand.cards : ', initialHouseHand.cards);
+        setPlayerHand(initialPlayerHand.cards);
+      } catch (error) {
+        console.error('Error shuffling deck : ', error);
+      }
+    } else {
+      console.error('No Deck Available');
+    }
+  };
+
+  // Implement current score calculator
+  // use score calculator to determine winner
+  // Implement Stand Functionality - essentially with the given parameters this will be a button
+  // to ensure that the player has a winning hand and offers a way to exit the game as a winner
+
   return (
     <div className='flex flex-col items-center mt-4 text-5xl'>
       <h1 className='p-4'> Blackjack Game </h1>
-      {/* <button
-        onClick={startGame}
-        className='border-rounded m-4 p-4 border-white border-4 bg-green-500 rounded-full text-lg'
-      >
-        Start Game
-      </button> */}
-      <div className='bg-green-500 rounded-lg border-4 border-white p-4 h-64 w-9/12 m-4'>
-        <Hand cards={playerHand} />
+      <div id='ButtonDiv' className='flex flex-row items-center'>
+        <button
+          onClick={hitPlayer}
+          className='px-4 mr-4 bg-green-500 rounded-lg border-4 border-white text-lg'
+        >
+          Hit
+        </button>
+        <button
+          onClick={shuffleDeck}
+          className='px-4 ml-4 bg-green-500 rounded-lg border-4 border-white text-lg'
+        >
+          SHUFFLE
+        </button>
+      </div>
+      <div className='bg-green-500 rounded-lg border-4 border-white p-4 h-64 w-9/12 m-4 w-auto'>
+        <h1 className='text-3xl'>House Hand</h1>
+        <Hand cards={houseHand} />
       </div>
       <div className='bg-green-500 rounded-lg border-4 border-white p-4 h-64 w-9/12 m-4'>
-        <Hand cards={houseHand} />
+        <h1 className='text-3xl'>Player Hand</h1>
+        <Hand cards={playerHand} />
       </div>
     </div>
   );
